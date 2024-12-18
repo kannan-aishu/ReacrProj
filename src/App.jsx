fbb2fa22e6e6af4e98229507b693446df8ff9247
Kannan G <130127328+kannan-aishu@users.noreply.github.com>
2024-12-18 11:42:29 +0000

    Auto commit on Wed Dec 18 11:42:29 UTC 2024

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Register_Page from './Components/Register_Page/Register_page'
+import Home from './Components/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Home/>
     <Register_Page/>
     </>
   )
